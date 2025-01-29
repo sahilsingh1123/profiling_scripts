d8a453e5a3fc738aae4193a8a69f5ede97e18b34
Sahil Singh <sahilsingh@Sahils-MacBook-Pro.local>
2025-01-29 05:39:37 +0530

    minor: Added code changes for using cProfile module

--- README.md
+++ README.md
@@ -4,3 +4,23 @@ Code for profiling python scripts
 This project is focused on exploring multiple ways of profiling python scripts
 <br>
 
+<h2>Tools <br></h2>
+<h3>Inbuilt <br></h3>
+- cProfile
+- timeit
+
+<h3>3rd party</h3>
+- snakeviz
+- tuna
+- line-profiler
+- memory-profiler
+- py-spy
+- pyinstrument
+- scalene
+
+
+<h2>Solutions</h2>
+- Memory leak identification
+- cython - compile python to C code
+- __slots__ - to reduce object overhead
+- Numba - JIT-compile functions for numerical code
---
+++ cProfiling.py
@@ -0,0 +1,50 @@
+import time
+import cProfile, pstats
+
+
+def slow_function():
+    time.sleep(1)
+    result = sum(i ** 2 for i in range(10_00000))
+    return result
+
+
+def fast_function():
+    time.sleep(1)
+    result = sum([i ** 2 for i in range(10_00000)])
+    return result
+
+
+def run_both_func():
+    slow_function()
+    fast_function()
+
+
+def run_terminal():
+    run_both_func()
+    run_both_func()
+
+def generate_perf_file():
+    with cProfile.Profile() as profile:
+        run_both_func()
+        statsData = pstats.Stats(profile)
+        statsData.sort_stats(pstats.SortKey.TIME)
+        statsData.print_stats()
+        statsData.dump_stats("cProfiling_stats.prof")  # tuna/snakeviz cProfiling_stats.prof
+
+if __name__ == "__main__":
+    # run_terminal()
+    generate_perf_file()
+
+"""
+# It can be done in this manner as well
+if __name__ == "__main__":
+    pr = cProfile.Profile()
+    pr.enable()
+    
+    slow_function()
+    fast_function()
+    
+    pr.disable()
+    pr.print_stats(sort="cumulative")
+    pr.dump_stats("stats.prof")
+"""
